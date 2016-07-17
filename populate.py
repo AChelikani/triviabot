ae7744194fe86508bdbe4fa55231d6c8f4cb7f07
Advith Chelikani <advith.chelikani@gmail.com>
2016-07-16 23:30:56 -0700

    question update

--- populate.py
+++ populate.py
@@ -1,3 +1,4 @@
+# Want to let bot pull from database
 import sqlite3
 
 conn = sqlite3.connect ('trivia.db')
@@ -10,9 +11,9 @@ c.execute('DROP TABLE trivia')
 c.execute('''CREATE TABLE trivia
              (q_id INTEGER PRIMARY KEY NOT NULL, question text, answer text, category text, value integer)''')
 
-c.execute('INSERT INTO trivia(question, answer, category, value) VALUES ("What is your name 1?", "Saanvi", "General", 200)')
-c.execute('INSERT INTO trivia(question, answer, category, value) VALUES ("What is your name 2?", "Saanvi", "General", 200)')
-c.execute('INSERT INTO trivia(question, answer, category, value) VALUES ("What is your name 3?", "Saanvi", "General", 200)')
+c.execute('INSERT INTO trivia(question, answer, category, value) VALUES ("What is your name 1?", "answer1", "General", 200)')
+c.execute('INSERT INTO trivia(question, answer, category, value) VALUES ("What is your name 2?", "answer2", "General", 300)')
+c.execute('INSERT INTO trivia(question, answer, category, value) VALUES ("What is your name 3?", "answer3", "General", 400)')
 
 conn.commit()
 
